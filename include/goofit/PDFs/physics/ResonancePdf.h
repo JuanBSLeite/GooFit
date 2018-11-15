3b40472f743e7ff46f71ed63d93aa292fd5fdfc4
Juan Leite <juanbsleite@live.com>
2018-11-15 20:30:36 -0200

    removing heaviside

--- include/goofit/PDFs/physics/ResonancePdf.h
+++ include/goofit/PDFs/physics/ResonancePdf.h
@@ -11,4 +11,4 @@
 #include <goofit/PDFs/physics/resonances/NonRes.h>
 #include <goofit/PDFs/physics/resonances/RBW.h>
 #include <goofit/PDFs/physics/resonances/Spline.h>
-#include <goofit/PDFs/physics/resonances/Heaviside.h>
+
