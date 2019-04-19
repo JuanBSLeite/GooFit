dd7f32d4b7d8ada303cab1ed9c0eb388f239e10d
Juan Leite <juanbsleite>
2019-04-19 01:16:13 -0300

    new method for dalitzplotter

--- include/goofit/PDFs/physics/DalitzPlotter.h
+++ include/goofit/PDFs/physics/DalitzPlotter.h
@@ -8,14 +8,18 @@
 #include <algorithm>
 #include <numeric>
 #include <random>
+#include <string>
 
 #if GOOFIT_ROOT_FOUND
 #include <TH2.h>
+#include <TH1.h>
+
 #endif
 
 namespace GooFit {
 
 /// This class makes it easy to make plots over 3 body Dalitz PDFs. You can use ROOT style value access or bin numbers.
+
 class DalitzPlotter {
     std::vector<size_t> xbins;
     std::vector<size_t> ybins;
@@ -25,14 +29,57 @@ class DalitzPlotter {
     EventNumber eventNumber;
     UnbinnedDataSet data;
     fptype mother;
+    GooPdf * overallSignal = nullptr;
+    DalitzPlotPdf * signalDalitz = nullptr;
+    
+  private:
+    double cpuGetM23(Variable massSum, double sij, double sik) { return (massSum.getValue() - sij - sik); }
+
+    #if GOOFIT_ROOT_FOUND
+    void drawFitPlotsWithPulls(TH1 *hd, TH1 *ht, std::string plotdir) {
+            
+            const char *hname = hd->GetName();
+            char obsname[10];
+            for(int i = 0;; i++) {
+                if(hname[i] == '_')
+                    obsname[i] = '\0';
+                else
+                    obsname[i] = hname[i];
+                if(obsname[i] == '\0')
+                    break;
+            }
+            ht->Scale(hd->Integral() / ht->Integral());
+            ht->SetLineColor(kRed);
+            ht->SetLineWidth(3);
+            //hd->SetMarkerStyle(15);
+
+            hd->SetMarkerColor(kBlue);
+            hd->SetFillColor(kBlue);
+            //hd->Rebin(20);
+
+
+            TCanvas foo;
+
+            hd->Draw("HIST");
+            ht->Draw("HIST  same");
+
+
+            foo.SaveAs( (plotdir+"/"+obsname+"_fit.png").c_str()    );
+
+
+        }
+        #endif
 
   public:
-    DalitzPlotter(GooPdf *overallSignal, DalitzPlotPdf *signalDalitz)
+    DalitzPlotter( GooPdf * overallSignal , DalitzPlotPdf * signalDalitz )
         : m12(signalDalitz->_m12)
         , m13(signalDalitz->_m13)
         , eventNumber(signalDalitz->_eventNumber)
         , data({m12, m13, eventNumber})
-        , mother(signalDalitz->decayInfo.motherMass) {
+        , mother(signalDalitz->decayInfo.motherMass)
+        , overallSignal(overallSignal)
+        , signalDalitz(signalDalitz)
+        {
         eventNumber.setValue(0);
 
         for(size_t i = 0; i < m12.getNumBins(); ++i) {
@@ -94,6 +141,9 @@ class DalitzPlotter {
         }
     }
 
+    
+    
+
     size_t getNumEvents() const { return data.getNumEvents(); }
 
     size_t getX(size_t event) const { return xbins.at(event); }
@@ -134,6 +184,121 @@ class DalitzPlotter {
         }
 
         return dalitzplot;
+    }
+
+    void Plot(double Mother_mass,double d1_mass,double d2_mass,double d3_mass, std::string sij, std::string sik , std::string sjk , std::string plotdir,UnbinnedDataSet data) {
+        
+
+        m12.setNumBins(120);
+        m13.setNumBins(120);
+        TH1F m12_dat_hist("m12_dat_hist", "", m12.getNumBins(), 0.01*m12.getLowerLimit(), m12.getUpperLimit());
+        m12_dat_hist.GetXaxis()->SetTitle( (sij+"[GeV]^{2}").c_str());
+        m12_dat_hist.GetYaxis()->SetTitle("Events");
+
+        TH1F m12_pdf_hist("m12_pdf_hist", "", m12.getNumBins(), 0.01*m12.getLowerLimit(), m12.getUpperLimit());
+
+        TH1F m13_dat_hist("m13_dat_hist", "", m13.getNumBins(), 0.01*m13.getLowerLimit(), m13.getUpperLimit());
+        m13_dat_hist.GetXaxis()->SetTitle( (sik+"[GeV]^{2}").c_str());
+        m13_dat_hist.GetYaxis()->SetTitle("Events");
+
+        TH1F m13_pdf_hist("m13_pdf_hist", "", m13.getNumBins(), m13.getLowerLimit(), m13.getUpperLimit());
+
+        TH1F m23_dat_hist("m23_dat_hist", "", m13.getNumBins(), 0.01*m13.getLowerLimit(), m13.getUpperLimit());
+        m23_dat_hist.GetXaxis()->SetTitle( (sjk+"[GeV]^{2}").c_str());
+        m23_dat_hist.GetYaxis()->SetTitle("Events");
+
+        TH1F m23_pdf_hist("m23_pdf_hist", "", m13.getNumBins(), 0.01*m13.getLowerLimit(), m13.getUpperLimit());
+
+        double totalPdf = 0;
+        double totalDat = 0;
+        TH2F dalitz_dat_hist("dalitz_data_hist",
+                                "",
+                                m12.getNumBins(),
+                                m12.getLowerLimit(),
+                                m12.getUpperLimit(),
+                                m13.getNumBins(),
+                                m13.getLowerLimit(),
+                                m13.getUpperLimit());
+        dalitz_dat_hist.SetStats(false);
+        dalitz_dat_hist.GetXaxis()->SetTitle((sij+"[GeV]^{2}").c_str());
+        dalitz_dat_hist.GetYaxis()->SetTitle((sik+"[GeV]^{2}").c_str());
+        TH2F dalitz_pdf_hist("dalitz_pdf_hist",
+                                "",
+                                m12.getNumBins(),
+                                m12.getLowerLimit(),
+                                m12.getUpperLimit(),
+                                m13.getNumBins(),
+                                m13.getLowerLimit(),
+                                m13.getUpperLimit());
+
+        dalitz_pdf_hist.GetXaxis()->SetTitle((sij+"[GeV]^{2}").c_str());
+        dalitz_pdf_hist.GetYaxis()->SetTitle((sik+"[GeV]^{2}").c_str());
+        dalitz_pdf_hist.SetStats(false);
+
+        int NevG = 1e7;
+
+        std::vector<Observable> vars;
+        vars.push_back(m12);
+        vars.push_back(m13);
+        
+        vars.push_back(eventNumber);
+        UnbinnedDataSet currData(vars);
+        int evtCounter = 0;
+
+        TRandom3 donram(50);
+        for(int i = 0; i < NevG; i++) {
+            do {
+                m12.setValue(donram.Uniform(m12.getLowerLimit(), m12.getUpperLimit()));
+                m13.setValue(donram.Uniform(m13.getLowerLimit(), m13.getUpperLimit()));
+            } while(!inDalitz(m12.getValue(), m13.getValue(), Mother_mass, d1_mass, d2_mass, d3_mass));
+
+                eventNumber.setValue(evtCounter);
+                evtCounter++;
+                currData.addEvent();
+        }
+
+        overallSignal->setData(&currData);
+        signalDalitz->setDataSize(currData.getNumEvents());
+        std::vector<std::vector<double>> pdfValues = overallSignal->getCompProbsAtDataPoints();
+
+        Variable massSum("massSum", POW2(Mother_mass) + POW2(d1_mass) + POW2(d2_mass) + POW2(d3_mass));
+
+
+        for(unsigned int j = 0; j < pdfValues[0].size(); ++j) {
+            double currm12 = currData.getValue(m12, j);
+            double currm13 = currData.getValue(m13, j);
+
+        
+            dalitz_pdf_hist.Fill(currm12, currm13, pdfValues[0][j]);
+            m12_pdf_hist.Fill(currm12, pdfValues[0][j]);
+            m13_pdf_hist.Fill(currm13, pdfValues[0][j]);
+            m23_pdf_hist.Fill( cpuGetM23(massSum,currm12, currm13) , pdfValues[0][j]);
+            totalPdf += pdfValues[0][j];
+        
+        }
+
+        TCanvas foo;
+        foo.SetLogz(false);
+        dalitz_pdf_hist.Draw("colz");
+
+        foo.SaveAs("plots/dalitz_pdf.png");
+
+        for(unsigned int evt = 0; evt < data.getNumEvents(); ++evt) {
+            double data_m12 = data.getValue(m12, evt);
+            m12_dat_hist.Fill(data_m12);
+            double data_m13 = data.getValue(m13, evt);
+            m13_dat_hist.Fill(data_m13);
+            m23_dat_hist.Fill(cpuGetM23(massSum,data_m12, data_m13));
+            dalitz_dat_hist.Fill(data_m12, data_m13);
+            
+            totalDat++;
+        }
+        dalitz_dat_hist.Draw("colz");
+        foo.SaveAs( (plotdir+"/dalitz_dat.png").c_str());
+
+        drawFitPlotsWithPulls(&m12_dat_hist, &m12_pdf_hist, plotdir);
+        drawFitPlotsWithPulls(&m13_dat_hist, &m13_pdf_hist, plotdir);
+        drawFitPlotsWithPulls(&m23_dat_hist, &m23_pdf_hist, plotdir);
     }
 #endif
 };
